e3fa9e08f0296682590a340b1b6d036d74c38400
brandonfang <bdmfang@gmail.com>
2021-06-14 12:38:31 -0700

    Fix post and patch routes

--- validation/events.js
+++ validation/events.js
@@ -12,8 +12,36 @@ module.exports = function validateEventInput(data) {
     errors.title = 'Title field is required';
   }
 
+  if (!Validator.isLength(data.title, { min: 1, max: 140 })) {
+    errors.title = 'Title must be between 1 and 140 characters';
+  }
+
   if (Validator.isEmpty(data.topic)) {
     errors.topic = 'Topic field is required';
+  }
+
+  if (!Validator.isLength(data.topic, { min: 1, max: 140 })) {
+    errors.topic = 'Topic must be between 1 and 140 characters';
+  }
+
+  if (!Validator.isLength(data.description, { min: 0, max: 1400 })) {
+    errors.description = 'Description must be between 0 and 1400 characters';
+  }
+
+  if (data.startTime || data.endTime) {
+    if (!data.startTime || !data.endTime) {
+      erros.endTime = 'You must enter both start time and end time.';
+      errors.endTime = 'You must enter both start time and end time.';
+    }
+
+    let currentTime = new Date();
+    if (data.startTime < currentTime) {
+      errors.startTime = 'Start time must be in the future.';
+    }
+
+    if (startTime > endTime) {
+      errors.endTime = 'End time must be after start time.'
+    }
   }
 
   return {
