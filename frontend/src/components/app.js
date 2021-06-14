b8daab7d97b3551a6c15e8b501bdea6413c2ac81
Josiah Leon <sjosiahleon@gmail.com>
2021-06-13 21:31:20 -0700

    did as much as I can do without backend support

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
+import LoginFormContainer from './session/login_form_container';
+import SignupFormContainer from './session/signup_form_container';
+import { Link } from 'react-router-dom';
+
+class temp extends React.Component {
+    render(){
+        return (
+            <div>
+                Welcome to this site, I guess...
+                <Link to="/login">login</Link>
+                <Link to="/Signup">Signup</Link>
+            </div>
+        )
+    }
+}
 
 
 const App = () => (
@@ -8,6 +23,7 @@ const App = () => (
       <Switch>
         <AuthRoute exact path="/login" component={LoginFormContainer} />
         <AuthRoute exact path="/signup" component={SignupFormContainer} />
+        <AuthRoute exact path="/" component={temp} />
       </Switch>
     </div>
   );
