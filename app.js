4333fcbe87197da8678c735e26fb23a77f3d1833
Inho Lee <inhojl@outlook.com>
2021-06-14 14:44:50 -0700

    got stream to work, testing on heroku now

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
+const { PeerServer } = require('peer');
 const express = require('express');
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server)
 const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path');
+const { v4: uuidV4 } = require('uuid');
 
 const users = require('./routes/api/users');
 const events = require('./routes/api/events');
 const User = require('./models/User');
+const e = require('express');
 
 
-mongoose
+mongoose 
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB successfully'))
   .catch((err) => console.log(err));
 
-// app.get('/', (req, res) => res.send('Hello World'));
-
-app.use(passport.initialize());
-require('./config/passport')(passport);
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
@@ -31,9 +29,47 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 // Add api routes
 app.use('/api/users', users);
 app.use('/api/events', events);
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+// Peer Server
+let peerServer;
+if (process.env.NODE_ENV === 'production') {
+  peerServer = new PeerServer({ 
+    secure: true,
+    host: 'mebrinjo.herokuapp.com',
+    port: 443, 
+  })
+} else {
+  peerServer = new PeerServer({ 
+    port: 9000, 
+    path: '/peer'
+  })
+}
+
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    console.log(roomId, userId)
+    socket.join(roomId)
+    socket.broadcast.to(roomId).emit('user-connected', userId)
+    socket.on('disconnect', () => {
+      socket.broadcast.to(roomId).emit('user-disconnected', userId)
+    })
+  })
+})
+
+
+server.listen(port, () => console.log(`Server is running on port ${port}`));
--- frontend/src/components/video/Video/Video.jsx
+++ frontend/src/components/video/Video/Video.jsx
@@ -1,14 +1,96 @@
-import React from 'react';
-import styles from './Video.module.scss';
+import React, { useState, useEffect, useRef } from 'react';
+import io from 'socket.io-client';
+import Peer from 'peerjs';
 
+
+import './Video.scss';
 
 const Video = () => {
 
+  const socket = useRef();
+
+  useEffect(() => {
+    socket.current = io.connect('/');
+    let myPeer;
+    if (process.env.NODE_ENV === 'production') {
+      myPeer = new Peer({
+        host: 'mebrinjo.herokuapp.com',
+        port: 443
+      })
+    } else {
+      myPeer = new Peer({
+        host: '/',
+        path: '/peer',
+        port: 9000
+      })
+    }
+
+    const videoGrid = document.getElementById('video-grid');
+    const myVideo = document.createElement('video');
+    myVideo.muted = true;
+
+    const peers = {};
+
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+    }).then(stream => {
+      addVideoStream(myVideo, stream)
+
+      myPeer.on('call', call => {
+        call.answer(stream);
+
+        const video = document.createElement('video');
+        call.on('stream', userVideoStream => {
+          addVideoStream(video, userVideoStream)
+        })
+      })
+
+      socket.current.on('user-connected', userId => {
+        console.log('user connected', userId)
+        connectToNewUser(userId, stream);
+      })
+      
+    })
+
+    socket.current.on('user-disconnected', userId => {
+      if (peers[userId]) peers[userId].close()
+    })
+
+    myPeer.on('open', id => {
+      socket.current.emit('join-room', 'room', id)
+    })
+
+
+    function connectToNewUser(userId, stream) {
+      const call = myPeer.call(userId, stream);
+      const video = document.createElement('video');
+      call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+      })
+      call.on('close', () => {
+        video.remove()
+      })
+      peers[userId] = call
+    
+    }
+    
+    function addVideoStream(video, stream) {
+      video.srcObject = stream;
+      video.addEventListener('loadedmetadata', (event) => {
+        console.log(event)
+        video.play()
+      })
+      videoGrid.append(video)
+    }
+  
+
+  }, [])
+
 
   return (
-    <div className={styles.video}>
-      VIDEO COMPONENT
-      <h1 className={styles.header}>HEADER</h1>
+    <div id='video-grid'>
+
     </div>
   );
 }
