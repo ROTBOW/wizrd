fcce35fd79b00e135d8beb1c79fc3c2a0bd59356
Josiah Leon <sjosiahleon@gmail.com>
2021-06-14 14:20:38 -0700

    Created Event creation form, needs to be tested with backend

--- frontend/src/components/events/EventForm/EventForm.js
+++ frontend/src/components/events/EventForm/EventForm.js
@@ -10,15 +10,25 @@ class EventForm extends React.Component {
             title: '',
             topic: '',
             description: '',
-            startDate: '',
+            startDate: this.getTime(),
+
+            liveToggle: false
 
         }
 
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleToggle = this.handleToggle.bind(this);
     }
 
     handleSubmit(e){
         e.preventDefault();
+        let submitForm = {
+          title: this.state.title,
+          topic: this.state.topic,
+          description: this.state.description,
+          startDate: this.state.startDate
+        };
+        this.props.createEvent(submitForm);
     }
 
     update(key) {
@@ -27,7 +37,41 @@ class EventForm extends React.Component {
       }
     }
 
+    addZeroIfNeeded(val) {
+      if (val < 10) {
+        return `0${val}`
+      }
+      return val;
+    }
+
+    getTime() {
+      let datetime = new Date();
+      let month = this.addZeroIfNeeded(datetime.getMonth()+1);
+      let day = this.addZeroIfNeeded(datetime.getDate());
+      let second = this.addZeroIfNeeded(datetime.getSeconds());
+
+      return `${datetime.getFullYear()}-${month}-${day}T${datetime.getHours()}:${second}`
+    }
+
+    handleToggle(e){
+      e.preventDefault();
+      this.setState({liveToggle: !this.state.liveToggle})
+    }
+
     render(){
+      
+      let liveToggle = (this.state.liveToggle) ? 
+      <button onClick={this.handleToggle}>
+        Schedule event for later
+      </button> : <div><input
+                    type='datetime-local'
+                    min={this.getTime()}
+                    value={this.state.startDate}
+                    onChange={this.update('startDate')}
+                  />
+                  <button onClick={this.handleToggle}>Switch to live Event</button>
+                  </div>
+
         return (
             <form onSubmit={this.handleSubmit}>
                 <h1>Create a Event</h1>
@@ -58,7 +102,7 @@ class EventForm extends React.Component {
                     />
                 </label>
 
-                <input type='datetime-local' min={Date.now()}></input>
+                {liveToggle}
 
                 <button type='submit'>Create Event</button>
             </form>
