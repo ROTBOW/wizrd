ed8b7e92329ec123adede3f0b4e42af59f5d4660
Melissa Flynn <mflynn814@gmail.com>
2021-06-15 13:48:47 -0700

    Fill out chat component

---
+++ frontend/src/components/chat/Chat.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState, useRef } from 'react';
+import io from 'socket.io-client';
+
+import styles from './Chat.module.scss';
+
+
+const Chat = (props) => {
+  const [messages, setMessages] = useState([]);
+  const [socket, setSocket] = useState(null);
+  
+  
+  useEffect(() => {
+    let newSocket;
+
+    if (!socket) {
+      newSocket = io.connect('/');
+      setSocket(newSocket);
+
+      newSocket.emit('join chat', {
+        chatId: props.chatId,
+        username: props.user.username
+      });
+    } else {
+      newSocket = socket;
+    }
+    
+    newSocket.on('new message', ({username, msg}) => {
+        let message = `${username}: ${msg}`;
+        setMessages([...messages, message]);
+      });
+
+    return () => newSocket.off('new message');
+
+  }, [messages])
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const input = document.getElementById("chat-input");
+    if (input.value) {
+      socket.emit('chat message', {chatId: props.chatId, msg: input.value, username: props.username})
+      input.value = '';
+    };
+  };
+
+  return (
+    <div>
+      <ul className={styles.chatList} id="message-list">
+        {messages.map((message, i) => {
+          return <li key={i}>{message}</li>;
+        })}
+      </ul>
+      <form>
+        <input type="text" id="chat-input"></input>
+      </form>
+      <button onClick={handleSubmit}>Send</button>
+    </div>
+  )
+}
+
+export default Chat;
