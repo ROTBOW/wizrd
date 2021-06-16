1d46dc476887760e33c8db37e41b644efc22865d
brandonfang <bdmfang@gmail.com>
2021-06-16 10:30:22 -0700

    Start home feed styling

--- frontend/src/components/home/SplashHome.jsx
+++ frontend/src/components/home/SplashHome.jsx
@@ -5,7 +5,7 @@ import styles from './SplashHome.module.scss';
 const SplashHome = (props) => {
 
   return (
-    <div style={{'maxWidth': '1024px', 'margin': '0 auto', 'padding': '32px 32px'}}>
+    <div style={{'maxWidth': '1080px', 'margin': '0 auto', 'padding': '32px 32px'}}>
         <h2 className={styles.title}>Welcome to this site</h2>
         <p><Link to="/login">Login</Link></p>
         <p><Link to="/signup">Sign up</Link></p>
