cb42bed9c2051d81f9f08548fa29908da6603735
Melissa Flynn <mflynn814@gmail.com>
2021-06-16 10:41:59 -0700

    Add Create Event button to home feed page

--- frontend/src/components/home/HomeFeed.jsx
+++ frontend/src/components/home/HomeFeed.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import styles from './HomeFeed.module.scss';
 import { fetchLiveEvents, fetchFutureEvents } from '../../util/eventUtil';
+import EventForm from '../events/EventForm/EventForm';
 
 const HomeFeed = (props) => {
   const [liveEvents, setLiveEvents] = useState([]);
@@ -20,33 +21,40 @@ const HomeFeed = (props) => {
   }, [])
 
   return (
-    <div className={styles.wrapper}>
-    
-      <div className={styles.sectionWrapper}> 
-        <h2>Currently Streaming Events</h2>
-        <ul>
-          {liveEvents ? liveEvents.map((e, i) => {
-            return <li key={i}>
-                <Link to={`/event/${e._id}`}><h5>{e.title}</h5></Link>
-                <p>{e.topic}</p>
-              </li>
-          }) : ''}
-        </ul>
-      </div>
+    <div>
+      {this.props.modal === 'createEvent' ? <EventForm /> : ''}
+      <div className={styles.wrapper}>
 
-      <div> 
-        <h2>Upcoming Events</h2>
-        <ul>
-          {futureEvents ? futureEvents.map((e, i) => {
-            return <li key={i}>
-                <Link to={`/event/${e._id}`}><h5>{e.title}</h5></Link>
-                <p>{e.topic}</p>
-                <p>{e.startTime}</p>
-              </li>
-          }) : ''}
-        </ul>
-      </div>
+        <div>
+          <button onClick={this.props.updateModal('createEvent')}>Create Event</button>
+        </div>
+      
+        <div className={styles.sectionWrapper}> 
+          <h2>Currently Streaming Events</h2>
+          <ul>
+            {liveEvents ? liveEvents.map((e, i) => {
+              return <li key={i}>
+                  <Link to={`/event/${e._id}`}><h5>{e.title}</h5></Link>
+                  <p>{e.topic}</p>
+                </li>
+            }) : ''}
+          </ul>
+        </div>
 
+        <div> 
+          <h2>Upcoming Events</h2>
+          <ul>
+            {futureEvents ? futureEvents.map((e, i) => {
+              return <li key={i}>
+                  <Link to={`/event/${e._id}`}><h5>{e.title}</h5></Link>
+                  <p>{e.topic}</p>
+                  <p>{e.startTime}</p>
+                </li>
+            }) : ''}
+          </ul>
+        </div>
+
+      </div>
 
     </div>
   );
@@ -55,12 +63,13 @@ const HomeFeed = (props) => {
 const mapStateToProps = (state, ownProps) => ({
   user: state.session.user,
   errors: state.errors.session,
+  modal: state.ui.modal,
   fetchLiveEvents: fetchLiveEvents,
   fetchFutureEvents: fetchFutureEvents
-
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
+  updateModal: (modalName) => dispatch(updateModal(modalName))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeFeed);
