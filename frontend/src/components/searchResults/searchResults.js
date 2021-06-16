9e1daec5bc7eff13668a8f95d2a718dcd92db69e
Melissa Flynn <mflynn814@gmail.com>
2021-06-16 16:54:28 -0700

    Search page working

--- frontend/src/components/searchResults/searchResults.js
+++ frontend/src/components/searchResults/searchResults.js
@@ -19,27 +19,29 @@ const SearchResults = (props) => {
   }, [results])
 
   return (
+
     <section className={styles.sectionWrapper}> 
-          <h2 className={styles.categoryTitle}>Currently Streaming Events</h2>
-          <ul className={styles.eventsGrid}>
-            {results ? results.map((e, i) => {
-              return <li key={i} className={styles.eventCard}>
-                  <Link to={`/event/${e._id}`} className={styles.noUnderline}>
-                    <h3 className={styles.eventTitle}>{e.title}</h3>
-                  </Link>
-                  <div className={styles.cardRowWrapper}>
-                    <BiBulb className={styles.cardIcon}/>
-                    <p className={styles.eventTopic}>{e.topic}</p>
-                  </div>
-                  <div className={styles.cardRowWrapper}>
-                    <BiVideo className={styles.cardIcon}/>
-                    <p className={styles.eventStartTime}>{moment(e.startTime).format("ddd, MMM D, LT")}</p>
-                  </div>
-                  {e.description ? <p className={styles.eventDescription}>{e.description}</p> : ''}
-                </li>
-            }) : ''}
-          </ul>
-        </section>
+      <h2 className={styles.categoryTitle}>Results</h2>
+      <ul className={styles.eventsGrid}>
+        {results ? results.map((e, i) => {
+          return <li key={i} className={styles.eventCard}>
+            <Link to={`/event/${e._id}`} className={styles.noUnderline}>
+              <h3 className={styles.eventTitle}>{e.title}</h3>
+            </Link>
+            <div className={styles.cardRowWrapper}>
+              <BiBulb className={styles.cardIcon}/>
+              <p className={styles.eventTopic}>{e.topic}</p>
+            </div>
+            <div className={styles.cardRowWrapper}>
+              <BiVideo className={styles.cardIcon}/>
+              <p className={styles.eventStartTime}>{moment(e.startTime).format("ddd, MMM D, LT")}</p>
+            </div>
+            {e.description ? <p className={styles.eventDescription}>{e.description}</p> : ''}
+          </li>
+          }) : ''}
+        </ul>
+      </section>
+
   );
 }
 
