db229fdf2db54fb466d58c9869a7b19bdc328f7c
Melissa Flynn <mflynn814@gmail.com>
2021-06-17 14:05:14 -0700

    Styling login modal

--- frontend/src/components/session/sessionModal/SessionModal.js
+++ frontend/src/components/session/sessionModal/SessionModal.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import LoginFormContent from '../LoginForm/LoginFormContent';
+import styles from './SessionModal.module.scss';
 
 const SessionModal = (props) => {
 
-  return 
+  return (
+    <div className={styles.formWrapper}>
+      <LoginFormContent errors={props.errors} login={props.login} />
+    </div>
+  )
 }
 
 export default SessionModal;
--- frontend/src/components/session/sessionModal/SessionModalContainer.js
+++ frontend/src/components/session/sessionModal/SessionModalContainer.js
@@ -1,5 +1,18 @@
 import { connect } from 'react-redux';
 import SessionModal from './SessionModal';
+import { login } from '../../../actions/sessionActions';
 
+const mapStateToProps = (state) => {
+  return {
+    errors: state.errors.session
+  };
+};
 
-export default connect(null, null)(SessionModal);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    login: (user) => dispatch(login(user))
+    // add demo login dispatch
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SessionModal);
