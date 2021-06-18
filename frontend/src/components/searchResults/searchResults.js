e549bc05311db62925c6417b37f5c95e070fee6b
Melissa Flynn <mflynn814@gmail.com>
2021-06-18 10:20:13 -0700

    horizontal scrolling on results

--- frontend/src/components/searchResults/searchResults.js
+++ frontend/src/components/searchResults/searchResults.js
@@ -42,32 +42,35 @@ const SearchResults = (props) => {
   return (
     <div>
       {modal}
-      <section className={styles.sectionWrapper}> 
-        <h2 className={styles.categoryTitle}>Results</h2>
-        <ul className={styles.eventsGrid}>
-          {errors ? <li className={styles.errorItem}>{errors}</li> :
-          results ? results.map((e, i) => {
-            return <a className={`${styles.noUnderline} ${styles.eventCard}`} key={i} onClick={() => auth(e._id)}>
-              <li>
-                <h3 className={styles.eventTitle}>{e.title}</h3>
-                <div className={styles.cardRowWrapper}>
-                  <BiBulb className={styles.cardIcon}/>
-                  <p className={styles.eventTopic}>{e.topic}</p>
-                </div>
-                <div className={styles.cardRowWrapper}>
-                  <BiVideo className={styles.cardIcon}/>
-                  <p className={styles.eventStartTime}>
-                    {moment(e.startTime).format("ddd, MMM D, LT")}
-                  </p>
-                </div>
-                
-                {e.description ? <p className={styles.eventDescription}>{e.description}</p> : ''}
-              </li>
-            </a>
-          }) : ''
-        }
-        </ul>
-      </section>
+      <main className={styles.feedWrapper}>
+
+        <section className={styles.sectionWrapper}> 
+          <h2 className={styles.categoryTitle}>Results</h2>
+          <ul className={styles.eventsGrid}>
+            {errors ? <li className={styles.errorItem}>{errors}</li> :
+            results ? results.map((e, i) => {
+              return <a className={`${styles.noUnderline} ${styles.eventCard}`} key={i} onClick={() => auth(e._id)}>
+                <li>
+                  <h3 className={styles.eventTitle}>{e.title}</h3>
+                  <div className={styles.cardRowWrapper}>
+                    <BiBulb className={styles.cardIcon}/>
+                    <p className={styles.eventTopic}>{e.topic}</p>
+                  </div>
+                  <div className={styles.cardRowWrapper}>
+                    <BiVideo className={styles.cardIcon}/>
+                    <p className={styles.eventStartTime}>
+                      {e.startTime > new Date() ? "Live now" : moment(e.startTime).format("ddd, MMM D, LT")}
+                    </p>
+                  </div>
+                  
+                  {e.description ? <p className={styles.eventDescription}>{e.description}</p> : ''}
+                </li>
+              </a>
+            }) : ''
+          }
+          </ul>
+        </section>
+      </main>
     </div>
 
   );
