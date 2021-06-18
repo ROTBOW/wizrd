8b73ae3768f76787e0f6a661147c5d0d9241cdc9
Josiah Leon <sjosiahleon@gmail.com>
2021-06-17 18:43:11 -0700

    added forest animal avatars

--- frontend/src/assets/avatars/avatars.js
+++ frontend/src/assets/avatars/avatars.js
@@ -1,13 +1,23 @@
 import red from './avatarRed.png';
 import green from './avatarGreen.png';
 import yellow from './avatarYellow.png';
+import deer from './deer.png';
+import fox from './fox.png';
+import hedgehog from './hedgehog.png';
+import owl from './owl.png';
+import racoon from './racoon.png'
 
 // the avatars work by importing them all above and exporting them as an array,
 // that way any componet that needs them can import this file, and the saved avatar number in the user should 
 // correspond to the avatar in this array
 
 export default [
-    red,
-    green,
-    yellow
+    fox,
+    deer,
+    hedgehog,
+    owl,
+    racoon
+    // green,
+    // yellow,
+    // red
 ];
